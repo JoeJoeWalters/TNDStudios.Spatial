e90f1135175db6f7420bbacfa17d89327682952e
TNDStudios <info@thenakeddeveloper.com>
2020-04-06 11:08:54 +0100

    Start of TCX Build Out

--- TNDStudios.Spatial/Documents/TCX.cs
+++ TNDStudios.Spatial/Documents/TCX.cs
@@ -7,20 +7,35 @@ using TNDStudios.Spatial.Common;
 using TNDStudios.Spatial.Helpers;
 
 /// <summary>
-/// https://www8.garmin.com/Sxmlschemas/TrainingCenterDatabasev2.xsd
+/// https://www8.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd
 /// </summary>
 namespace TNDStudios.Spatial.Documents
 {
     /// <summary>
-    /// Implementation of https://www8.garmin.com/Sxmlschemas/TrainingCenterDatabasev2.xsd
+    /// Implementation of https://www8.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd
     /// </summary>
     [Serializable]
-    [XmlRoot("gpx", Namespace = "http://www.topografix.com/GPX/1/1")]
-    public class TCXFile : IGeoFileConvertable
+    [XmlRoot("TrainingCenterDatabase", Namespace = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2")]
+    public class TCXFile : XmlBase, IGeoFileConvertable
     {
+        [XmlElement("Activities")]
+        public TCXActivities Activities { get; set; }
+
         public GeoFile ToGeoFile()
         {
             throw new NotImplementedException();
         }
+    }
+
+    public class TCXActivities : XmlBase
+    {
+        [XmlElement("Activity")]
+        public List<TCXActivity> Items { get; set; }
+    }
+
+    public class TCXActivity : XmlBase
+    {
+        [XmlAttribute("Sport")]
+        public String Sport { get; set; }
     }
 }
