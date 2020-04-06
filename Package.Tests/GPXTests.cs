958d7ab751c137102b3654401859a7a2daf513fd
TNDStudios <info@thenakeddeveloper.com>
2020-04-06 01:39:46 +0100

    Added back in GPX Tests but specifically to check conversions and not basic geographic functions such as speed which are now handled in the Geography tests

---
+++ Package.Tests/GPXTests.cs
@@ -0,0 +1,35 @@
+using TNDStudios.Spatial.Documents;
+using TNDStudios.Spatial.Helpers;
+using TNDStudios.Spatial.Types;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Newtonsoft.Json;
+
+namespace TNDStudios.Spatial.Tests
+{
+    public class GPXTests : TestBase
+    {
+        private readonly GPXFile gpxTrackFile;
+
+        public GPXTests()
+        {
+            gpxTrackFile = base.GetXMLData<GPXFile>("GPXFiles/GPXRouteOnly.gpx");
+        }
+
+        [Fact]
+        public void Track_Compare_Conversion()
+        {
+            // ARRANGE
+            Int32 origionalCount = 0;
+            Int32 transformedCount = 0;
+
+            // ACT
+            origionalCount = gpxTrackFile.Tracks[0].TrackSegments[0].TrackPoints.Count; // Count of origional
+            transformedCount = gpxTrackFile.ToGeoFile().Routes[0].Points.Count; // Do conversion and count
+
+            // ASSERT
+            Assert.Equal(origionalCount, transformedCount);
+        }
+    }
+}
