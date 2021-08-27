df6186dc9f5528b601e5e036ed4012c7ec78436e
joe walters <joewalters999@gmail.com>
2021-08-27 23:53:22 +0100

    Moved GeoFile to sub-folder and split up class to multiple files

--- Spatial.Package/Documents/GeoFile/GeoFile.cs
+++ Spatial.Package/Documents/GeoFile/GeoFile.cs
@@ -24,21 +24,4 @@ namespace TNDStudios.Spatial.Documents
         /// </summary>
         public List<GeoFileRoute> Routes { get; set; }
     }
-
-    /// <summary>
-    /// Common route format for internal processing
-    /// </summary>
-    public class GeoFileRoute
-    { 
-        /// <summary>
-        /// Name of the route
-        /// </summary>
-        public String Name { get; set; }
-
-        /// <summary>
-        /// List of ordered geographic points to indicate the path of the route
-        /// </summary>
-        public List<GeoCoordinateExtended> Points { get; set; }
-    }
-
 }
