590fdf6e8f5265fef3e1b7d5b9ddacec7ceffbcf
Joe Walters <info@thenakeddeveloper.com>
2023-01-15 21:59:27 +0000

    Example Skia render

--- src/Spatial.RenderEngine/Renderer.cs
+++ src/Spatial.RenderEngine/Renderer.cs
@@ -5,6 +5,32 @@ namespace Spatial.RenderEngine
 {
     public class Renderer
     {
+        public Renderer()
+        {
 
+        }
+
+        // https://washamdev.com/skiasharp-getting-started-and-simple-tutorial/
+        public void Generate()
+        {
+            SKImageInfo imageInfo = new SKImageInfo(300, 250);
+            using (SKSurface surface = SKSurface.Create(imageInfo))
+            {
+                SKCanvas canvas = surface.Canvas;
+
+                //canvas.DrawColor(SKColors.Red);
+                canvas.Clear();
+                canvas.Clear(SKColors.Empty); //same thing but also erases anything else on the canvas first
+
+                using (SKPaint paint = new SKPaint())
+                {
+                    paint.Color = SKColors.Blue;
+                    paint.IsAntialias = true;
+                    paint.StrokeWidth = 15;
+                    paint.Style = SKPaintStyle.Stroke;
+                    canvas.DrawCircle(50, 50, 30, paint); //arguments are x position, y position, radius, and paint
+                }
+            }
+        }
     }
 }
